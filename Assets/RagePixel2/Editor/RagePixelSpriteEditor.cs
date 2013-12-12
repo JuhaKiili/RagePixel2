0b9db5f922d1a48246e2092ff1f656d7cd0cdc4f
Juha Kiili <juha@juhakiili.com>
2013-12-12 23:17:32 +0200

    Initial commit

---
+++ Assets/RagePixel2/Editor/RagePixelSpriteEditor.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(RagePixelSprite))]
+public class RagePixelSpriteEditor : Editor 
+{
+	public override void OnInspectorGUI ()
+	{
+		
+	}
+
+	public void OnSceneGUI ()
+	{
+		
+	}
+}
---
+++ Assets/RagePixel2/Runtime/RagePixelSprite.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class RagePixelSprite : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
